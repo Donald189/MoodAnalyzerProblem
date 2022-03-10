dedefdcdfa18315b58167df28b79c17bd6f3d078
Donald189 <donaldkarey@gmail.com>
2022-03-10 20:37:39 +0530

    Donald TC 1.1 Refactored

--- AnalyzerTest.cs
+++ AnalyzerTest.cs
@@ -15,28 +15,10 @@ namespace MoodAnalyzerTest
             // Arrange
             string expected = "SAD";
             string message = "I am in Sad Mood";
-            MoodAnalyse moodAnalyse = new MoodAnalyse();
+            MoodAnalyse moodAnalyse = new MoodAnalyse(message);
 
             // Act
-            string mood = moodAnalyse.AnalyseMood(message);
-
-            // Assert
-            Assert.AreEqual(expected, mood);
-        }
-
-        /// <summary>
-        /// TC 1.2: Given “I am in Any Mood” message Should Return HAPPY
-        /// </summary>
-        [TestMethod]
-        public void GivenAnyMoodShouldReturnHAPPY()
-        {
-            // Arrange
-            string expected = "HAPPY";
-            string message = "I am in Any Mood";
-            MoodAnalyse moodAnalyse = new MoodAnalyse();
-
-            // Act
-            string mood = moodAnalyse.AnalyseMood(message);
+            string mood = moodAnalyse.AnalyseMood();
 
             // Assert
             Assert.AreEqual(expected, mood);
